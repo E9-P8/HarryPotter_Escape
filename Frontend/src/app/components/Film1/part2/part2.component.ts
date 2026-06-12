30a5665076f63d03a5c06f34d9f5513569bc72e0
Elisa98-hash <puorto.elisa@hotmail.com>
2026-06-12 12:32:33 +0800

    added game logic

--- Frontend/src/app/components/Film1/part2/part2.component.ts
+++ Frontend/src/app/components/Film1/part2/part2.component.ts
@@ -31,9 +31,21 @@ export class Part2Component implements OnInit {
   showlightHouseVideo: boolean = false;
   islightHouseDialogueEnd : boolean = false;
   islightHouseEnigmaSolved : boolean = false;
+  isDoorVibrating: boolean = false;
   currentLinelightHouse: number = 0;
   doorClicks: number = 0;
 
+  isFirePlaceSolved: boolean = false;
+  correctFireplaceSequence: string[] = ['oil', 'match']; 
+  userFireplaceSequence: string[] = [];
+  showSmoke: boolean = false;
+
+  isUmbrellaSolved: boolean = false;
+  isUmbrellaGameActive: boolean = false;
+  cursorPosition: number = 0; 
+  cursorDirection: number = 1; 
+  umbrellaInterval: any;
+  isDudleyTransformed: boolean = false;
 
   isPubDialogueEnd : boolean = false;
   isPubEnigmaSolved : boolean = false;
@@ -78,9 +90,9 @@ export class Part2Component implements OnInit {
         text : "Esprimi un desiderio Harry"
       },
       { 
-        character : '',
+        character : '*TOC TOC*',
         image: "assets/img/Part2/lightHouse_emptyDoor.png", //bottone da cliccare 6 volte 
-        text : ""
+        text : "*Qualcuno sta bussando alla porta*"
       },
       { 
         character : 'Hagrid',
@@ -89,47 +101,76 @@ export class Part2Component implements OnInit {
       },
       { 
         character: 'Zio vernon', 
-        image: "assets/img/Part2/",
+        image: "assets/img/Part2/ZioFucile.png",
         text: "Se ne vada immediatamente signore! Questa è un'effrazione!!"
       },
       { 
         character : 'Hagrid',
-        image: "assets/img/Part2/",
+        image: "assets/img/Part2/HagridFucile.png",
         text : "Essiccati Sursley, vecchia prugna (?)"//suono di sparo
       },
       { 
         character : 'Hagrid',
         image: "assets/img/Part2/HarryCake.png", 
-        text : "Tieni Harry, ho qualcosa per te. Mica tutti i giorni un giovanotto compie 11 anni eh!"
+        text : "Tieni Harry, ho qualcosa per te. Mica tutti i giorni un giovanotto compie 11 anni eh!" //accende fuoco
       },
       { 
+        character : 'Hagrid', 
+        image: "assets/img/Part2/FireOff.png", //fuoco spento
+        text : ""
+      },
+      { 
+        character : 'Hagrid', 
+        image: "assets/img/Part2/FireOn.png",//fuoco acceso
+        text : ""
+      },
+      { 
+        character : 'Hagrid', //dialogo hagrid e harry
+        image: "assets/img/Part2/",
+        text : ""
+      },
+      { 
+        character : 'Hagrid', //dialogo hagrid e harry
+        image: "assets/img/Part2/",
+        text : ""
+      },{ 
         character : 'Hagrid', //mettere lettera di benvenuto
         image: "assets/img/Part2/",
         text : ""
       },
       { 
         character : 'Zio Vernon',
-        image: "",
+        image: "assets/img/Part2/",
         text : "Lui non ci andrà! Lo abbiamo promesso quando l'abbiamo preso!"
       },
       { 
         character : 'Hagrid',
-        image: "",
+        image: "assets/img/Part2/",
         text : "Questo ragazzo è iscritto dal giorno in cui è nato! Andrà alla miglior scuola di magia e stregoneria del mondo e sarà sotto il miglior preside che Hogwarts ha mai visto!.. Albus Silente!"
       },
       { 
         character : 'Zio Vernon',
-        image: "",
+        image: "assets/img/Part2/",
         text : "Non intendo pagare perchè un vecchio strampalato insegni trucchi di magia"
       },
       { 
         character : 'Hagrid',
-        image: "",
+        image: "assets/img/Part2/",
         text : "Mai insultare Albus Silente davanti a me!" //mettere incantesimo coda panico generale
       },
       { 
+        character : '',
+        image: "assets/img/Part2/DudleyNotTrasformed.png.png", 
+        text : "" 
+      },
+      { 
+        character : '',
+        image: "assets/img/Part2/DudleyTrasformed.png",
+        text : "" 
+      },
+      { 
         character : 'Hagrid',
-        image: "",
+        image: "assets/img/Part2/",
         text : "Oh! Siamo molto in ritardo, dobbiamo andare!"
       }
   ]
@@ -147,31 +188,119 @@ export class Part2Component implements OnInit {
         //timeout parte la battuta
         return;
       }
+      if (this.currentLinelightHouse === 6 && !this.isFirePlaceSolved) {
+        return;
+      }
+      if (this.currentLinelightHouse === 14 && !this.isUmbrellaSolved) {
+        this.startUmbrellaGame();
+        return;
+      }
       setTimeout(() => {
         this.currentLinelightHouse++;
         this.startlightHousePubDialogue()
           }, 1000);
   }
 
-    onDoorClick() {
-      if (this.currentLinelightHouse === 1 && this.doorClicks < 6) {
-        this.doorClicks++;
-        this.audioService.playSound('');
-
-        if (this.doorClicks === 6) { 
+      onDoorClick() {
+        if (this.currentLinelightHouse === 1 && this.doorClicks < 6) {
+          this.doorClicks++;
           this.audioService.playSound('');
-          this.currentLinelightHouse++;
-          this.startlightHousePubDialogue();
+
+          this.isDoorVibrating = true;
+
+          setTimeout(() => {
+                this.isDoorVibrating = false;
+              }, 150); 
+
+          if (this.doorClicks === 6) { 
+            this.audioService.playSound('');
+            this.currentLinelightHouse++;
+            this.startlightHousePubDialogue();
+          }
         }
       }
+      useFireplaceItem(item: 'wood' | 'oil' | 'match' | 'wetNews') {
+          if (this.isFirePlaceSolved) return;
+          this.userFireplaceSequence.push(item);
+          
+          this.audioService.playSound('item_click'); 
+
+          if (this.userFireplaceSequence.length === 2) {
+            
+            const isSequenceCorrect = this.userFireplaceSequence[0] === this.correctFireplaceSequence[0] &&
+                                      this.userFireplaceSequence[1] === this.correctFireplaceSequence[1];
+
+            if (isSequenceCorrect) {
+              this.isFirePlaceSolved = true;
+              this.audioService.playSound('fire_burst'); // Suono fiammata
+              this.userFireplaceSequence = []; // Svuota
+              
+              this.currentLinelightHouse = 7;
+              this.startlightHousePubDialogue();
+            } else {
+              this.audioService.playSound('smoke_fail'); 
+              this.showSmoke = true;
+              this.userFireplaceSequence = []; 
+
+              setTimeout(() => {
+                this.showSmoke = false;
+              }, 2500);
+            }
+          }
+        }
+      startUmbrellaGame() {
+          this.isUmbrellaGameActive = true;
+          this.cursorPosition = 0;
+          this.cursorDirection = 1;
+
+          this.umbrellaInterval = setInterval(() => {
+            this.cursorPosition += 3 * this.cursorDirection; 
+
+            if (this.cursorPosition >= 100) {
+              this.cursorPosition = 100;
+              this.cursorDirection = -1; 
+            } else if (this.cursorPosition <= 0) {
+              this.cursorPosition = 0;
+              this.cursorDirection = 1; 
+            }
+          }, 10);
+        }
+      onUmbrellaShoot() {
+        if (!this.isUmbrellaGameActive || this.isUmbrellaSolved) return;
+
+        const minWinZone = 40;
+        const maxWinZone = 60;
+
+        if (this.cursorPosition >= minWinZone && this.cursorPosition <= maxWinZone) {
+          clearInterval(this.umbrellaInterval);
+          this.isUmbrellaGameActive = false;
+          this.isUmbrellaSolved = true;
+          
+          this.isDudleyTransformed = true;
+          this.audioService.playSound('magic_shoot'); 
+          this.currentLinelightHouse = 15;
+
+          setTimeout(() => {
+            this.currentLinelightHouse = 16; 
+            this.audioService.playSound('pig_squeak'); 
+          }, 1000);
+
+        setTimeout(() => {
+            this.isDudleyTransformed = false; 
+            this.currentLinelightHouse = 17; 
+          }, 2500);
+      } else {
+      this.audioService.playSound('spark_fail'); 
     }
-  followHagrid(){
-    this.actualFase= 'diagonAlley';
-    setTimeout(()=>{
-      this.startPubDialogue();
-    },1000)
-    
     }
+
+      followHagrid(){
+        this.actualFase= 'diagonAlley';
+        setTimeout(()=>{
+          this.startPubDialogue();
+        },1000)
+        
+        }
  
   /***************** DIAGON ALLEY ************************ */
   script2 : pubDialogue[] = [
